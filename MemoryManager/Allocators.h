8d86ae28a3f5493a72728f30906e5f671099bd42
Victor Torres <60521761+vtantonio20@users.noreply.github.com>
2022-11-13 02:58:00 -0500

    Add files via upload

---
+++ MemoryManager/Allocators.h
@@ -0,0 +1,7 @@
+
+int bestFit(int sizeInWords, void *list);
+int worstFit(int sizeInWords, void *list);
+
+int bestFitVector(int sizeInWords, void *list);
+int worstFitVector(int sizeInWords, void *list);
+
