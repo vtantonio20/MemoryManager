1b158568e2ab3960ff5a0166baa320b2720c3dc2
Victor Torres <60521761+vtantonio20@users.noreply.github.com>
2022-11-13 02:55:44 -0500

    Delete Allocators.h

--- Allocators.h
+++
@@ -1,7 +0,0 @@
-
-int bestFit(int sizeInWords, void *list);
-int worstFit(int sizeInWords, void *list);
-
-int bestFitVector(int sizeInWords, void *list);
-int worstFitVector(int sizeInWords, void *list);
-
